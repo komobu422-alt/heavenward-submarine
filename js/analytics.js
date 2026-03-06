1daae38ca8602f24be8c03637ea5b7d02b224d82
KO <ko@beizinopasokon.local>
2026-03-06 18:03:25 +0900

    changed SNS review images and add analytics.js

---
+++ js/analytics.js
@@ -0,0 +1,9 @@
+const script = document.createElement('script');
+script.async = true;
+script.src = "https://www.googletagmanager.com/gtag/js?id=G-9ML95CZJ7V";
+document.head.appendChild(script);
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-9ML95CZJ7V');
